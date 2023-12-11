c40d8c487327f5ddce3424e74186ee802f2998da
JordyVandemoortele <125571637+JordyVandemoortele@users.noreply.github.com>
2023-12-11 15:48:03 +0100

    Initial commit

---
+++ PeopleManager.Ui.BlazorApp/Program.cs
@@ -0,0 +1,27 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using PeopleManager.Sdk.Extensions;
+using PeopleManager.Ui.BlazorApp;
+using PeopleManager.Ui.BlazorApp.Providers;
+using PeopleManager.Ui.BlazorApp.Stores;
+using Vives.Authentication.Abstractions;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<ITokenStore, TokenStore>();
+builder.Services.AddApi("https://localhost:7134");
+
+builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+
+builder.Services.AddCascadingAuthenticationState();
+
+await builder.Build().RunAsync();
---
+++ PeopleManager.Ui.BlazorApp/Providers/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+using Newtonsoft.Json.Linq;
+using Vives.Authentication.Abstractions;
+using Vives.Security.Jwt;
+
+namespace PeopleManager.Ui.BlazorApp.Providers
+{
+    public class CustomAuthenticationStateProvider(ITokenStore tokenStore): AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            //Get bearer from TokenStore
+            var token = await tokenStore.GetToken();
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                return new AuthenticationState(new ClaimsPrincipal());
+            }
+
+            //Convert token to ClaimsPrincipal
+            var claimsPrincipal = JwtSecurityHelper.GetClaimsPrincipal(token, "Bearer");
+
+            return new AuthenticationState(claimsPrincipal);
+        }
+    }
+}
